54eb596cc01faab20b77562e926e8fd8cc2b920e
eschiffi00 <emanuelschiffini@grupoambient.com.ar>
2022-10-03 17:04:49 -0300

    update 03/10

--- AmbientHouse/Reportes/ReporteEventosConfirmados.aspx.cs
+++ AmbientHouse/Reportes/ReporteEventosConfirmados.aspx.cs
@@ -7,7 +7,9 @@ using System.Web.UI.WebControls;
 using DomainAmbientHouse.Servicios;
 using DomainAmbientHouse.Entidades;
 using System.Web.UI.HtmlControls;
-
+using DbEntidades.Entities;
+using DbEntidades.Operators;
+using DBEntidades.Entities;
 
 namespace AmbientHouse.Reportes
 {
@@ -47,11 +49,51 @@ namespace AmbientHouse.Reportes
 
             //HtmlTable table =  GenerarTabla(ListPresupuestosConfirmados.ToList().Count(), 3);
             //Panel1.Controls.Add(table);
-
-            GridViewEventosConfirmados.DataSource = ListPresupuestosConfirmados.ToList();
+            List<NewEventosConfirmados> eventosNew = new List<NewEventosConfirmados>();
+            eventosNew = CargarNuevoReporte(ListPresupuestosConfirmados.ToList());
+            GridViewEventosConfirmados.DataSource = eventosNew;
             GridViewEventosConfirmados.DataBind();
         }
-
+        public List<NewEventosConfirmados> CargarNuevoReporte(List<ObtenerEventos> lista)
+        {
+            List<NewEventosConfirmados> EventosConfirmadosNew = new List<NewEventosConfirmados>();
+            NewEventosConfirmados e = new NewEventosConfirmados();
+            foreach(var evento in lista) { 
+                e.ApellidoNombre = evento.ApellidoNombre;
+                e.CARACTERISTICA = evento.CARACTERISTICA;
+                e.LOCACION = evento.LOCACION;
+                e.SECTOR = evento.SECTOR;
+                e.JORNADA = evento.JORNADA;
+                e.HorarioEvento = evento.HorarioEvento;
+                e.CantidadInicialInvitados = evento.CantidadInicialInvitados;
+                e.FechaEvento = evento.FechaEvento;
+                e.Id = evento.Id;
+                e.Estado = evento.Estado;
+                e.Ejecutivo = evento.Ejecutivo;
+                e.EventoId = evento.EventoId;
+                e.ClienteId = evento.ClienteId;
+                e.EmpleadoId = evento.EmpleadoId;
+                e.EstadoId = evento.EstadoId;
+                e.RazonSocial = evento.RazonSocial;
+                e.Fecha = evento.Fecha;
+                e.PresupuestoId = evento.PresupuestoId;
+                e.PresupuestoEstadoId = evento.PresupuestoEstadoId;
+                e.EstadoPresupuesto = evento.EstadoPresupuesto;
+                e.PresupuestoIdAnterior = evento.PresupuestoIdAnterior;
+                e.LocacionId = evento.LocacionId;
+                e.EmpleadoCliente = evento.EmpleadoCliente;
+                e.TipoEvento = evento.TipoEvento;
+                e.ClienteBisId = evento.ClienteBisId;
+                e.Cliente = evento.Cliente;
+                e.FechaSena = evento.FechaSena;
+                e.TipoEventoId = evento.TipoEventoId;
+                var producto = ProductosOperator.GetOneByIdentity(PresupuestoDetalleOperator.GetOneByIdentity(e.PresupuestoId).ProductoId);
+                e.TipoExperiencia = TipoCateringOperator.GetOneByIdentity(producto.TipoCateringId.Value).Descripcion == null ? " " : TipoCateringOperator.GetOneByIdentity(producto.TipoCateringId.Value).Descripcion;
+                e.TipoBarra = TiposBarrasOperator.GetOneByIdentity(producto.TipoBarraId.Value).Descripcion == null ? " " : TiposBarrasOperator.GetOneByIdentity(producto.TipoBarraId.Value).Descripcion;
+                EventosConfirmadosNew.Add(e);
+            }
+            return EventosConfirmadosNew.ToList();
+        }
         private HtmlTable GenerarTabla(int filas, int columnas)
         {
             HtmlTable tabla = new HtmlTable();
